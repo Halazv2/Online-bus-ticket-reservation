55aabfe34331c701719c0aa2d11ec98b679b0f9e
Halazv2 <halaziani2003@gmail.com>
2022-10-11 22:33:32 +0100

    verify sign up if email used

---
+++ src/middlewares/verifySignUp.js
@@ -0,0 +1,30 @@
+const db = require("../models");
+const ROLES = db.ROLES;
+const User = db.user;
+
+checkDuplicateEmail = (req, res, next) => {
+    // Email
+    User.findOne({
+      email: req.body.email,
+    }).exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
+
+      if (user) {
+        res.status(400).send({ message: "Failed! Email is already in use!" });
+        return;
+      }
+
+      next();
+    });
+};
+
+
+
+const verifySignUp = {
+  checkDuplicateEmail,
+};
+
+module.exports = verifySignUp;
