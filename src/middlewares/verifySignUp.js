9511ac4574395159955b5d8a172008f19e5f227a
Halazv2 <halaziani2003@gmail.com>
2022-10-11 22:34:51 +0100

    check if role is other then roles in modul

--- src/middlewares/verifySignUp.js
+++ src/middlewares/verifySignUp.js
@@ -21,10 +21,24 @@ checkDuplicateEmail = (req, res, next) => {
     });
 };
 
+checkRolesExisted = (req, res, next) => {
+  if (req.body.roles) {
+    for (let i = 0; i < req.body.roles.length; i++) {
+      if (!ROLES.includes(req.body.roles[i])) {
+        res.status(400).send({
+          message: `Failed! Role ${req.body.roles[i]} does not exist!`,
+        });
+        return;
+      }
+    }
+  }
 
+  next();
+};
 
 const verifySignUp = {
   checkDuplicateEmail,
+  checkRolesExisted,
 };
 
 module.exports = verifySignUp;
