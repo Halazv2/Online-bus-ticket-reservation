51baec3f2e48cf7a90cda2a48b263abec31c8c97
Halazv2 <halaziani2003@gmail.com>
2022-10-11 22:07:39 +0100

    route auth

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const { verifySignUp } = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+
+const url = "/api/v1/mekna7";
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    `${url}/signup`,
+    [verifySignUp.checkDuplicateEmail, verifySignUp.checkRolesExisted],
+    controller.signup
+  );
+  app.post(`${url}/signin`, controller.signin);
+};
