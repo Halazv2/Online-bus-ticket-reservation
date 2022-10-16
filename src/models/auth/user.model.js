e71bbc331858c89fd0e18ace9b6ebcd1d4ea13cd
Halazv2 <halaziani2003@gmail.com>
2022-10-16 12:49:35 +0100

    change the schema to better one

--- src/models/auth/user.model.js
+++ src/models/auth/user.model.js
@@ -1,12 +1,39 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const User = mongoose.model(
   "users",
   new mongoose.Schema({
-    full_name: String,
-    age: Number,
-    email: String,
-    password: String,
+    full_name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 100,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: true,
+      unique: true,
+      validate(value) {
+        return validator.isEmail(value);
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
     // ref basically means that mongoose would store the ObjectId values
     // and when you call populate using those ObjectIds would fetch and fill the documents for you.
     roles: [
