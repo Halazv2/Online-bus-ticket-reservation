eac6bb5f8287d6b6857092ca5e2bea491cff5fbd
Halazv2 <halaziani2003@gmail.com>
2022-10-28 21:08:08 +0100

    admin dashboard

--- frontend/src/partials/header/UserMenu.jsx
+++ frontend/src/partials/header/UserMenu.jsx
@@ -42,7 +42,7 @@ function UserMenu() {
       >
         <img className="w-8 h-8 rounded-full" src={UserAvatar} width="32" height="32" alt="User" />
         <div className="flex items-center truncate">
-          <span className="truncate ml-2 text-sm font-medium group-hover:text-slate-800">Acme Inc.</span>
+          <span className="truncate ml-2 text-sm font-medium group-hover:text-slate-800 mr-2">Hafid</span>
           <svg className="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400" viewBox="0 0 12 12">
             <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
           </svg>
@@ -64,8 +64,8 @@ function UserMenu() {
           onFocus={() => setDropdownOpen(true)}
           onBlur={() => setDropdownOpen(false)}
         >
-          <div className="pt-0.5 pb-2 px-3 mb-1 border-b border-slate-200">
-            <div className="font-medium text-slate-800">Acme Inc.</div>
+          <div className="pt-0.5 pb-2 px-3 mb-1 border-b border-slate-200 ">
+            <div className="font-medium text-slate-800">Hafid</div>
             <div className="text-xs text-slate-500 italic">Administrator</div>
           </div>
           <ul>
--- frontend/src/utils/Utils.js
+++ frontend/src/utils/Utils.js
@@ -9,11 +9,11 @@ export const hexToRGB = (h) => {
   let r = 0;
   let g = 0;
   let b = 0;
-  if (h.length === 4) {
+  if (h.length == 4) {
     r = `0x${h[1]}${h[1]}`;
     g = `0x${h[2]}${h[2]}`;
     b = `0x${h[3]}${h[3]}`;
-  } else if (h.length === 7) {
+  } else if (h.length == 7) {
     r = `0x${h[1]}${h[2]}`;
     g = `0x${h[3]}${h[4]}`;
     b = `0x${h[5]}${h[6]}`;
