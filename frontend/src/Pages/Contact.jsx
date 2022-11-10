cf26db2d66b3957d81f4c253835b176f4d1ce6c7
Halazv2 <halaziani2003@gmail.com>
2022-11-10 15:24:47 +0100

    contatct us page

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -5,166 +5,104 @@ const Contact = () => {
   return (
     <div classNameName="container my-24 px-6 mx-auto">
       <IndexNavbar />
-        <section className="mb-32 text-gray-800">
+      <section className="mb-32 text-gray-800">
+        <div
+          className="relative overflow-hidden bg-no-repeat bg-cover"
+          style={{
+            backgroundImage: `url(https://www.sustainable-bus.com/wp-content/uploads/2021/05/J4500_CHARGE.jpg)`,
+            height: "500px",
+            backgroundPosition: "50%",
+            margin: "20px",
+            borderRadius: "10px",
+          }}
+        ></div>
+        <div className="container text-gray-800 px-4 md:px-12">
           <div
-            className="relative overflow-hidden bg-no-repeat bg-cover"
+            className="block rounded-lg shadow-lg py-10 md:py-12 px-4 md:px-6"
             style={{
-              backgroundImage: `url(https://www.sustainable-bus.com/wp-content/uploads/2021/05/J4500_CHARGE.jpg)`,
-              height: "500px",
-              backgroundPosition: "50%",
-              margin: "20px",
-              borderRadius: "10px",
+              marginTop: "-100px",
+              background: "hsla(0, 0%, 100%, 0.8)",
+              backdropFilter: "blur(10px)",
             }}
-          ></div>
-          <div className="container text-gray-800 px-4 md:px-12">
-            <div
-              className="block rounded-lg shadow-lg py-10 md:py-12 px-4 md:px-6"
-              style={{
-                marginTop: "-100px",
-                background: "hsla(0, 0%, 100%, 0.8)",
-                backdropFilter: "blur(10px)",
-              }}
-            >
-              <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 mb-12">
-                <div className="mb-12 lg:mb-0 text-center mx-auto">
-                  <svg
-                    className="w-8 h-8 text-blue-600 mb-6 mx-auto"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 496 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm82.29 357.6c-3.9 3.88-7.99 7.95-11.31 11.28-2.99 3-5.1 6.7-6.17 10.71-1.51 5.66-2.73 11.38-4.77 16.87l-17.39 46.85c-13.76 3-28 4.69-42.65 4.69v-27.38c1.69-12.62-7.64-36.26-22.63-51.25-6-6-9.37-14.14-9.37-22.63v-32.01c0-11.64-6.27-22.34-16.46-27.97-14.37-7.95-34.81-19.06-48.81-26.11-11.48-5.78-22.1-13.14-31.65-21.75l-.8-.72a114.792 114.792 0 0 1-18.06-20.74c-9.38-13.77-24.66-36.42-34.59-51.14 20.47-45.5 57.36-82.04 103.2-101.89l24.01 12.01C203.48 89.74 216 82.01 216 70.11v-11.3c7.99-1.29 16.12-2.11 24.39-2.42l28.3 28.3c6.25 6.25 6.25 16.38 0 22.63L264 112l-10.34 10.34c-3.12 3.12-3.12 8.19 0 11.31l4.69 4.69c3.12 3.12 3.12 8.19 0 11.31l-8 8a8.008 8.008 0 0 1-5.66 2.34h-8.99c-2.08 0-4.08.81-5.58 2.27l-9.92 9.65a8.008 8.008 0 0 0-1.58 9.31l15.59 31.19c2.66 5.32-1.21 11.58-7.15 11.58h-5.64c-1.93 0-3.79-.7-5.24-1.96l-9.28-8.06a16.017 16.017 0 0 0-15.55-3.1l-31.17 10.39a11.95 11.95 0 0 0-8.17 11.34c0 4.53 2.56 8.66 6.61 10.69l11.08 5.54c9.41 4.71 19.79 7.16 30.31 7.16s22.59 27.29 32 32h66.75c8.49 0 16.62 3.37 22.63 9.37l13.69 13.69a30.503 30.503 0 0 1 8.93 21.57 46.536 46.536 0 0 1-13.72 32.98zM417 274.25c-5.79-1.45-10.84-5-14.15-9.97l-17.98-26.97a23.97 23.97 0 0 1 0-26.62l19.59-29.38c2.32-3.47 5.5-6.29 9.24-8.15l12.98-6.49C440.2 193.59 448 223.87 448 256c0 8.67-.74 17.16-1.82 25.54L417 274.25z"
-                    />
-                  </svg>
-                  <h6 className="font-medium">
-                    Africa
-                  </h6>
-                </div>
-                <div className="mb-12 lg:mb-0 text-center mx-auto">
-                  <svg
-                    className="w-8 h-8 text-blue-600 mb-6 mx-auto"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 384 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"
-                    ></path>
-                  </svg>
-                  <h6 className="font-medium">New York, 94126</h6>
-                </div>
-                <div className="mb-6 md:mb-0 text-center mx-auto">
-                  <svg
-                    className="w-8 h-8 text-blue-600 mb-6 mx-auto"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"
-                    ></path>
-                  </svg>
-                  <h6 className="font-medium">+ 01 234 567 89</h6>
-                </div>
-
+          >
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-6 mb-12">
+              <div className="mb-12 lg:mb-0 text-center mx-auto">
+                <svg
+                  className="w-8 h-8 text-blue-600 mb-6 mx-auto"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 496 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm82.29 357.6c-3.9 3.88-7.99 7.95-11.31 11.28-2.99 3-5.1 6.7-6.17 10.71-1.51 5.66-2.73 11.38-4.77 16.87l-17.39 46.85c-13.76 3-28 4.69-42.65 4.69v-27.38c1.69-12.62-7.64-36.26-22.63-51.25-6-6-9.37-14.14-9.37-22.63v-32.01c0-11.64-6.27-22.34-16.46-27.97-14.37-7.95-34.81-19.06-48.81-26.11-11.48-5.78-22.1-13.14-31.65-21.75l-.8-.72a114.792 114.792 0 0 1-18.06-20.74c-9.38-13.77-24.66-36.42-34.59-51.14 20.47-45.5 57.36-82.04 103.2-101.89l24.01 12.01C203.48 89.74 216 82.01 216 70.11v-11.3c7.99-1.29 16.12-2.11 24.39-2.42l28.3 28.3c6.25 6.25 6.25 16.38 0 22.63L264 112l-10.34 10.34c-3.12 3.12-3.12 8.19 0 11.31l4.69 4.69c3.12 3.12 3.12 8.19 0 11.31l-8 8a8.008 8.008 0 0 1-5.66 2.34h-8.99c-2.08 0-4.08.81-5.58 2.27l-9.92 9.65a8.008 8.008 0 0 0-1.58 9.31l15.59 31.19c2.66 5.32-1.21 11.58-7.15 11.58h-5.64c-1.93 0-3.79-.7-5.24-1.96l-9.28-8.06a16.017 16.017 0 0 0-15.55-3.1l-31.17 10.39a11.95 11.95 0 0 0-8.17 11.34c0 4.53 2.56 8.66 6.61 10.69l11.08 5.54c9.41 4.71 19.79 7.16 30.31 7.16s22.59 27.29 32 32h66.75c8.49 0 16.62 3.37 22.63 9.37l13.69 13.69a30.503 30.503 0 0 1 8.93 21.57 46.536 46.536 0 0 1-13.72 32.98zM417 274.25c-5.79-1.45-10.84-5-14.15-9.97l-17.98-26.97a23.97 23.97 0 0 1 0-26.62l19.59-29.38c2.32-3.47 5.5-6.29 9.24-8.15l12.98-6.49C440.2 193.59 448 223.87 448 256c0 8.67-.74 17.16-1.82 25.54L417 274.25z"
+                  />
+                </svg>
+                <h6 className="font-medium">Africa</h6>
               </div>
-              <div className="max-w-[700px] mx-auto">
-                <form>
-                  <div className="form-group mb-6">
-                    <input
-                      type="text"
-                      className="form-control block
-                      w-full
-                      px-3
-                      py-1.5
-                      text-base
-                      font-normal
-                      text-gray-700
-                      bg-white bg-clip-padding
-                      border border-solid border-gray-300
-                      rounded
-                      transition
-                      ease-in-out
-                      m-0
-                      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                      id="exampleInput7"
-                      placeholder="Name"
-                    />
-                  </div>
-                  <div className="form-group mb-6">
-                    <input
-                      type="email"
-                      className="form-control block
-                      w-full
-                      px-3
-                      py-1.5
-                      text-base
-                      font-normal
-                      text-gray-700
-                      bg-white bg-clip-padding
-                      border border-solid border-gray-300
-                      rounded
-                      transition
-                      ease-in-out
-                      m-0
-                      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                      id="exampleInput8"
-                      placeholder="Email address"
-                    />
-                  </div>
-                  <div className="form-group mb-6">
-                    <textarea
-                      className="
-                      form-control
-                      block
-                      w-full
-                      px-3
-                      py-1.5
-                      text-base
-                      font-normal
-                      text-gray-700
-                      bg-white bg-clip-padding
-                      border border-solid border-gray-300
-                      rounded
-                      transition
-                      ease-in-out
-                      m-0
-                      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                    "
-                      id="exampleFormControlTextarea13"
-                      rows="3"
-                      placeholder="Message"
-                    ></textarea>
-                  </div>
-                  <button
-                    type="submit"
-                    className="
-                    w-full
-                    px-6
-                    py-2.5
-                    bg-blue-600
-                    text-white
-                    font-medium
-                    text-xs
-                    leading-tight
-                    uppercase
-                    rounded
-                    shadow-md
-                    hover:bg-blue-700 hover:shadow-lg
-                    focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-                    active:bg-blue-800 active:shadow-lg
-                    transition
-                    duration-150
-                    ease-in-out"
-                  >
-                    Send
-                  </button>
-                </form>
+              <div className="mb-12 lg:mb-0 text-center mx-auto">
+                <svg
+                  className="w-8 h-8 text-blue-600 mb-6 mx-auto"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"
+                  ></path>
+                </svg>
+                <h6 className="font-medium">New York, 94126</h6>
+              </div>
+              <div className="mb-6 md:mb-0 text-center mx-auto">
+                <svg
+                  className="w-8 h-8 text-blue-600 mb-6 mx-auto"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"
+                  ></path>
+                </svg>
+                <h6 className="font-medium">+ 01 234 567 89</h6>
               </div>
             </div>
+            <div className="max-w-[700px] mx-auto">
+              <form>
+                <div className="form-group mb-6">
+                  <input
+                    type="text"
+                    className="form-control block   w-full   px-3   py-1.5   text-base   font-normal   text-gray-700   bg-white bg-clip-padding   border border-solid border-gray-300   rounded   transition   ease-in-out   m-0   focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    id="exampleInput7"
+                    placeholder="Name"
+                  />
+                </div>
+                <div className="form-group mb-6">
+                  <input
+                    type="email"
+                    className="form-control block   w-full   px-3   py-1.5   text-base   font-normal   text-gray-700   bg-white bg-clip-padding   border border-solid border-gray-300   rounded   transition   ease-in-out   m-0   focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    id="exampleInput8"
+                    placeholder="Email address"
+                  />
+                </div>
+                <div className="form-group mb-6">
+                  <textarea
+                    className="   form-control   block   w-full   px-3   py-1.5   text-base   font-normal   text-gray-700   bg-white bg-clip-padding   border border-solid border-gray-300   rounded   transition   ease-in-out   m-0   focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none "
+                    id="exampleFormControlTextarea13"
+                    rows="3"
+                    placeholder="Message"
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className=" w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Send
+                </button>
+              </form>
+            </div>
           </div>
-        </section>
+        </div>
+      </section>
       <FooterSmall />
     </div>
   );
