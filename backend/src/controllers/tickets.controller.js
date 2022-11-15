33bca04485a927917c3c822319841396681b2c49
Halazv2 <halaziani2003@gmail.com>
2022-11-15 11:50:13 +0100

    fix : Create a Ticket add some stuff needed to be added and move saveseat seat after verfication of body req

--- backend/src/controllers/tickets.controller.js
+++ backend/src/controllers/tickets.controller.js
@@ -8,8 +8,8 @@ const trips = dbtrips.admin;
 // Create and Save a new Ticket
 exports.create = async (req, res) => {
   // Validate request
-  if (!req.body.trip_id) {
-    res.status(400).send({ message: "Content can not be empty!" });
+  if (!Object.keys(req.body).length === 10) {
+    res.status(400).send({ message: "Content can not be empty! éé" });
     return;
   }
 
@@ -17,6 +17,10 @@ exports.create = async (req, res) => {
   const ticket = new Tickets({
     trip_id: req.body.trip_id,
     user_id: req.body.user_id,
+    tripInfo: {
+      from: req.body.from,
+      to: req.body.to,
+    },
     user_info: {
       name: req.body.name,
       email: req.body.email,
@@ -24,18 +28,13 @@ exports.create = async (req, res) => {
     seat_number: req.body.seat_number,
     status: req.body.status,
     phoneNmber: req.body.phoneNmber,
+    price: req.body.price,
   });
 
   const user = await User.findById(req.body.user_id);
   const passangerInfo = {
-    name: req.body.user_id  ? user.full_name : req.body.name,
+    name: req.body.user_id ? user.full_name : req.body.name,
     email: req.body.user_id ? user.email : req.body.email,
-  };
-
-  // const trip = await trips.findById(req.body.trip_id);
-  const tripInfo = {
-    from: req.body.from,
-    to: req.body.to,
   };
 
   const seat = await checkSeat(
@@ -45,10 +44,10 @@ exports.create = async (req, res) => {
     req.body.trip_id
   );
   if (!seat) {
-    saveSeat(req, res, req.body.seat_number, req.body.trip_id);
     ticket
       .save(ticket)
       .then((data) => {
+        saveSeat(req, res, req.body.seat_number, req.body.trip_id);
         res.send({
           message: "Your ticket has been booked successfully",
           id: data._id,
@@ -57,7 +56,8 @@ exports.create = async (req, res) => {
           seat_number: data.seat_number,
           status: data.status,
           passangerInfo: passangerInfo,
-          tripInfo: tripInfo,
+          tripInfo: data.tripInfo,
+          price: data.price,
         });
       })
       .catch((err) => {
@@ -130,5 +130,37 @@ const saveSeat = async (req, res, seat_number, trip_id) => {
           message: `Cannot update trip with id=${id}. Maybe trip was not found!`,
         });
       } else return;
+    });
+};
+
+// get ticket by id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  Tickets.findById(id)
+    .then(async (data) => {
+      if (!data)
+        res.status(404).send({ message: "Not found Ticket with id " + id });
+      else {
+        const user = (await User.findById(data.user_id)) || data.user_info;
+        const passangerInfo = {
+          name: user.full_name || user.name,
+          email: user.email,
+        };
+
+        res.send({
+          id: data._id,
+          user_info: passangerInfo,
+          tripInfo: data.tripInfo,
+          trip_id: data.trip_id,
+          seat_number: data.seat_number,
+          status: data.status,
+          price: data.price,
+        });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Ticket with id=" + id });
     });
 };
