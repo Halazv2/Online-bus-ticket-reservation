b97e86fdf5ed729a010b5d468e0fb5a2b5a08f23
Halazv2 <halaziani2003@gmail.com>
2022-11-15 11:56:02 +0100

    check if user is authenticated

--- frontend/src/redux/auth.js
+++ frontend/src/redux/auth.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isAuthenticated: false,
+  isAuthenticated: localStorage.getItem("token") ? true : false,
   user: {},
 };
 
