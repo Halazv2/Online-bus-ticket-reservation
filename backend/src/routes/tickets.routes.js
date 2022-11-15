3214a3419a7eab1ad7cb821353dd37b6b8ab91aa
Halazv2 <halaziani2003@gmail.com>
2022-11-15 11:45:10 +0100

    add : Allow Origin && Methods to routes

--- backend/src/routes/tickets.routes.js
+++ backend/src/routes/tickets.routes.js
@@ -8,7 +8,9 @@ module.exports = function (app) {
   app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      "x-access-token, Origin, Content-Type, Accept",
+      "Access-Control-Allow-Origin: localhost:3000",
+      "Access-Control-Allow-Methods: GET, POST, OPTIONS"
     );
     next();
   });
@@ -16,4 +18,5 @@ module.exports = function (app) {
   app.post(`${url}/tickets`, controller.create);
   app.get(`${url}/tickets`, controller.findAll);
   app.post(`${url}/sendMail`, sendMailController.sendMail);
+  app.get(`${url}/getTicketByID/:id`, controller.findOne);
 };
--- backend/src/routes/trip.routes.js
+++ backend/src/routes/trip.routes.js
@@ -6,7 +6,9 @@ module.exports = function (app) {
   app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      "x-access-token, Origin, Content-Type, Accept",
+      "Access-Control-Allow-Origin: localhost:3000",
+      "Access-Control-Allow-Methods: GET, POST, OPTIONS"
     );
     next();
   });
