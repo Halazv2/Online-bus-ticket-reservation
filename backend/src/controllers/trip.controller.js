9c6d705d41f0200e956bc95e11ffc280e0638e81
Halazv2 <halaziani2003@gmail.com>
2022-11-15 11:55:19 +0100

    get the price of the trip by calculate the diff between time and multiplite * 30

--- backend/src/controllers/trip.controller.js
+++ backend/src/controllers/trip.controller.js
@@ -27,59 +27,81 @@ exports.getTripById = (req, res) => {
 };
 
 exports.filterTrips = (req, res) => {
-  const { depart, arrive } = req.body;
+  const { depart, arrive, deperture_date, passangers } = req.body;
+  const getPrice = (start, end) => {
+    let second = "00";
+    start = start.split(":");
+    end = end.split(":");
+
+    let startDate = new Date(2020, 5, 5, start[0], start[1], second);
+    let endDate = new Date(2020, 5, 5, end[0], end[1], second);
+    let difference = endDate.getTime() - startDate.getTime();
+    difference = difference / 1000;
+    let hourDifference = Math.floor(difference / 3600);
+    difference -= hourDifference * 3600;
+    let minuteDifference = Math.floor(difference / 60);
+    difference -= minuteDifference * 60;
+    minuteDifference = minuteDifference / 60;
+    let diff = minuteDifference + hourDifference;
+    // return only 2 numbers after the dot (ex: 1.5)
+    let price = 30 * diff;
+    return price.toFixed(2);
+  };
   Admin.find({
     destanition: { $elemMatch: { cities: { $all: [depart, arrive] } } },
+    deperture_date: { $eq: deperture_date },
   }).then((data) => {
-    if (depart && arrive) {
-      if (data == "") {
-        res.status(404).send({
-          message:
-            "there is no trip from " +
-            depart +
-            " to " +
-            arrive +
-            " please try another trip",
+    if (depart && arrive && deperture_date) {
+      if (
+        data.map(
+          (trip) =>
+            trip.destanition[0].cities.indexOf(depart) <
+            trip.destanition[0].cities.indexOf(arrive)
+        )
+      ) {
+        let timeofdepart = data.map((trip) => {
+          return trip.destanition[0].date[
+            trip.destanition[0].cities.indexOf(depart)
+          ];
         });
-      } else {
-        if (
-          data[0].destanition[0].cities.indexOf(depart) <
-          data[0].destanition[0].cities.indexOf(arrive)
-        ) {
+        let timeofarrive = data.map((trip) => {
+          return trip.destanition[0].date[
+            trip.destanition[0].cities.indexOf(arrive)
+          ];
+        });
+        // console.log(timeofdepart + " and " + timeofarrive);
+        const getP = getPrice(timeofdepart[0], timeofarrive[0]);
+        const filteredTrips = data.filter(
+          (trip) => trip.reserved_seats.length + passangers <= trip.seats
+        );
+        if (filteredTrips.length > 0) {
           res.send({
-            message:
-              "there is " +
-              data.length +
-              " trips from " +
-              depart +
-              " to " +
-              arrive,
-            data: data.map((item) => {
+            message: "there is " + filteredTrips.length + " trips",
+            data: filteredTrips.map((trip) => {
               return {
-                id: item._id,
+                id: trip._id,
                 destanition: [
                   {
                     cities: [depart, arrive],
                   },
                 ],
-                trip_type: item.trip_type,
-                price: item.price,
-                trip_status: item.trip_status,
-                seats: item.seats,
-                reserved_seats: item.reserved_seats,
+                deperture_date: trip.deperture_date,
+                timeofdepart: timeofdepart,
+                price: getP,
+                seats: trip.seats - trip.reserved_seats.length,
+                reserved_seats: trip.reserved_seats.length,
+                trip_type: trip.trip_type,
               };
             }),
           });
         } else {
-          res.status(404).send({
-            message:
-              "there is no trip from " +
-              depart +
-              " to " +
-              arrive +
-              " please try another trip",
-          });
+          res.status(404).send({ message: "No trips found" });
         }
+      } else {
+        res.status(404).send({
+          message:
+            "there are no trips with this destanition please try another trip",
+        });
       }
     } else {
       res.status(404).send({
