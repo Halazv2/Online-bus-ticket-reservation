7b0c824950b04eb29bb99ab9f055aba6e9ec1e0d
Halazv2 <halaziani2003@gmail.com>
2022-11-19 16:50:23 +0100

    protected route on admin pages -> verify token

--- frontend/src/ProtectedRoutes.js
+++ frontend/src/ProtectedRoutes.js
@@ -1,13 +1,36 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
-
+import axios from "axios";
+import React, { useState } from "react";
 const ProtectedRoutes = () => {
   const token = localStorage.getItem("accessToken") || null;
   const decoded = token ? jwt_decode(token) : null;
+  const userEmail = localStorage.getItem("userEmail") || null;
+  const verifyisAdmin = async () => {
+    return axios
+      .post("http://localhost:5000/api/v1/mekna7/verifyisAdmin", {
+        email: userEmail,
+      })
+      .then((res) => {
+        return res.data.roles[0];
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  if (!decoded || decoded.exp * 1000 < Date.now() - 24 * 60 * 60 * 1000) {
-    return <Navigate to="/login" />;
-  }
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    verifyisAdmin().then((role) => {
+      if (
+        !decoded ||
+        (decoded.exp * 1000 < Date.now() - 24 * 60 * 60 * 1000 &&
+          role !== "ROLE_ADMIN")
+      )
+        navigate("/Unauthorized");
+    });
+  }, []);
   return <Outlet />;
 };
 
